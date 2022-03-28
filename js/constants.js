2ac5d9b1b68793daf9d2079713d841ec10bcba71
Anna Haskelsky <annahaskelsky@gmail.com>
2022-03-28 19:22:55 +0300

    remove comments and fixed xhr request

--- js/constants.js
+++ js/constants.js
@@ -2,7 +2,7 @@ const PUBLISHER_ID = 'taboola-templates'
 const APP_TYPE = 'desktop'
 const API_KEY = 'f9040ab1b9c802857aa783c469d0e0ff7e7366e4'
 
-export const URL = `http://api.taboola.com/1.0/json/${PUBLISHER_ID}/recommendations.get?
+export const URL = `https://api.taboola.com/1.0/json/${PUBLISHER_ID}/recommendations.get?
 app.type=${APP_TYPE}&app.apikey=${API_KEY}&count=8&source.type=video&source.id=
 11111&source.url=http://www.site.com/videos/214321562187.html`
 
--- js/services/data.service.js
+++ js/services/data.service.js
@@ -15,41 +15,6 @@ function getData(onSuccess, onFailure, url = URL) {
     xhr.send()
 }
 
-
-// function getData() {
-//     // const cacheData = storageService.loadFromStorage('data')
-//     // if (cacheData) {
-//     //     console.log('from cache', cacheData);
-//     //     return Promise.resolve(JSON.parse(cacheData))
-//     // }
-//     return new Promise((resolve, reject) => {
-//         const xhr = new XMLHttpRequest()
-//         xhr.open('GET', URL);
-//         xhr.onreadystatechange = function () {
-//             if (xhr.readyState !== 4) return;
-//             const resp = JSON.parse(xhr.responseText);
-//             if (xhr.status === 200) {
-//                 //     storageService.saveToStorage('data', xhr.responseText)
-//                 //     resolve(JSON.parse(xhr.responseText))
-//                 // }
-//                 // storageService.saveToStorage('data', xhr.responseText)
-//                 resolve(resp);
-//             } else {
-//                 reject(resp.error || 'Couldn\'t fetch data');
-//             }
-//             // if (resp.error) {
-//             //     reject(resp.error);
-//             // }
-//             // else {
-//             //     storageService.saveToStorage('data', xhr.responseText)
-//             //     resolve(resp);
-//             // }
-
-//         }
-//         xhr.send();
-//     })
-// }
-
 export const dataService = {
     getData
 }
